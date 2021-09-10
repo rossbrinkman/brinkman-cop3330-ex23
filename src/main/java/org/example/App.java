6645778e6347922aa9926ab5a48edd9b5fab5ff8
rossbrinkman <55204801+rossbrinkman@users.noreply.github.com>
2021-09-10 11:54:02 -0400

    initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.Scanner;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Ross Brinkman
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "What is the first number? " );
+
+        Scanner scanner = new Scanner(System.in);
+        String first, second;
+        int firstNumber, secondNumber;
+
+        first = scanner.nextLine();
+        firstNumber = Integer.parseInt(first);
+
+        System.out.println( "What is the second number? " );
+        second = scanner.nextLine();
+        secondNumber = Integer.parseInt(second);
+
+        System.out.println( firstNumber + " + " + secondNumber + " = " + (firstNumber+secondNumber));
+        System.out.println( firstNumber + " - " + secondNumber + " = " + (firstNumber-secondNumber));
+        System.out.println( firstNumber + " * " + secondNumber + " = " + (firstNumber*secondNumber));
+        System.out.println( firstNumber + " / " + secondNumber + " = " + (firstNumber/secondNumber));
+    }
+}
