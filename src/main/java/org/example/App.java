97b106d49941abd57b7ef97c8d96337c7fb43a88
rossbrinkman <55204801+rossbrinkman@users.noreply.github.com>
2021-09-10 11:56:17 -0400

    finished 05

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,11 @@
 package org.example;
-
 import java.util.Scanner;
+
 /*
  *  UCF COP3330 Fall 2021 Assignment 1 Solution
  *  Copyright 2021 Ross Brinkman
  */
+
 public class App 
 {
     public static void main( String[] args )
