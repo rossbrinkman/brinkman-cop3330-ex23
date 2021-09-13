1ca3383d17202a4a5ed19aa1cba3e9417afe3f1c
rossbrinkman <55204801+rossbrinkman@users.noreply.github.com>
2021-09-13 12:36:02 -0400

    unfinished 17

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,24 +11,48 @@ public class App
 {
     public static void main( String[] args )
     {
-        DecimalFormat f = new DecimalFormat("##.00");
+        DecimalFormat f = new DecimalFormat("#0.00####");
         Scanner scanner = new Scanner(System.in);
-        float principal, interestRate, years, finalValue;
+        int maleOrFemale;
+        float ouncesAlcohol, weight, hoursSinceLastDrink, alcoholDistributionRatio, bloodAlcoholContent;
+        String currentInput;
 
-        System.out.println( "Enter the principal: " );
-        principal = scanner.nextFloat();
+        System.out.println( "Enter a 1 if you are male or a 2 if you are female: " );
+        currentInput = scanner.nextLine();
+        try{
+            maleOrFemale = Integer.parseInt(currentInput);}
+        catch (NumberFormatException nfe){
+            System.out.println( "Input must be numeric" );
+            return;
+        }
 
-        System.out.println( "Enter the rate of interest: " );
-        interestRate = scanner.nextFloat();
+        if(maleOrFemale == 1)
+            alcoholDistributionRatio = 0.73f;
+        else if (maleOrFemale == 2)
+            alcoholDistributionRatio = 0.66f;
+        else
+        {
+            System.out.println( "Invalid Input" );
+            return;
+        }
 
-        System.out.println( "Enter the number of years: " );
-        years = scanner.nextFloat();
+        System.out.println( "How many ounces of alcohol did you have? " );
+        ouncesAlcohol = scanner.nextFloat();
 
-        interestRate /= 100;
-        finalValue = principal * (1 + interestRate*years);
-        interestRate *= 100;
+        System.out.println( "What is your weight in pounds? " );
+        weight = scanner.nextFloat();
 
-        System.out.println( "After " + (int)years + " years at " + interestRate +
-                "%, the investment will be worth $" + f.format(finalValue));
+        System.out.println( "How many hours has it been since your last drink? " );
+        hoursSinceLastDrink = scanner.nextFloat();
+
+        bloodAlcoholContent = (ouncesAlcohol * 5.14f / weight * alcoholDistributionRatio) - .015f * hoursSinceLastDrink;
+
+        System.out.println( "Your BAC is " + f.format(bloodAlcoholContent));
+
+        String endString = bloodAlcoholContent >= .08f ? "It is not legal for you to drive" : "It is legal for you to drive";
+
+        System.out.println(endString);
+
+        scanner.close();
     }
 }
